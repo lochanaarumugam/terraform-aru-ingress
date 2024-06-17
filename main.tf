d84ffaf90feb2ebd19609be0363103b8d5153152
lochanaarumugam <arucary@gmail.com>
2024-06-17 14:12:15 -0400

    test

--- main.tf
+++ main.tf
@@ -2,7 +2,18 @@ provider "aws" {
   region = var.region
 }
 
-module "aru" {
+module "input" {
+  source = "./modules/sqs"
+  
+  queue_name             = var.queue_name
+  visibility_timeout     = var.visibility_timeout
+  message_retention      = var.message_retention
+  receive_wait_time      = var.receive_wait_time
+  environment            = var.environment
+  project                = var.project
+}
+
+module "output" {
   source = "./modules/sqs"
   
   queue_name             = var.queue_name
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,20 @@
-output "sqs_queue_id" {
+output "sqs_queue_id1" {
   description = "The ID of the SQS queue"
-  value       = module.aru.sqs_queue_id
+  value       = module.input.sqs_queue_id
 }
 
-output "sqs_queue_arn" {
+output "sqs_queue_arn1" {
   description = "The ARN of the SQS queue"
-  value       = module.aru.sqs_queue_arn
+  value       = module.input.sqs_queue_arn
+}
+
+
+output "sqs_queue_id2" {
+  description = "The ID of the SQS queue"
+  value       = module.output.sqs_queue_id
+}
+
+output "sqs_queue_arn2" {
+  description = "The ARN of the SQS queue"
+  value       = module.output.sqs_queue_arn
 }
